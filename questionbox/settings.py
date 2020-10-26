bae192e839f4022c2161d83e3d40d28a99c84918
Charlette Bauer <charlettebauer@aol.com>
2020-10-26 18:05:03 -0400

    first attempt

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     # Third-party
     'debug_toolbar',
     'django_extensions',
-
+    'registration', 
     # Project-specific
     'users',
 ]
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core'
             ],
         },
     },
